3b886f30e6ed5e52f36d13bd9e491314fc77e980
zhzhh <zhihongzhang_hi@163.com>
2018-07-15 21:09:56 +0800

    springboot jpa mysql with  single datasource

--- src/main/java/com/example/springboot/SpringDemoApplication.java
+++ src/main/java/com/example/springboot/SpringDemoApplication.java
@@ -3,7 +3,7 @@ package com.example.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class SpringDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringDemoApplication.class, args);
--- src/main/java/com/example/springboot/service/PersonService.java
+++ src/main/java/com/example/springboot/service/PersonService.java
@@ -1,18 +1,19 @@
-package com.example.springboot.springbootdemo;
+package com.example.springboot.service;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import com.example.springboot.entity.Person;
+import com.example.springboot.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.stereotype.Service;
 
 import javax.sql.DataSource;
+import javax.transaction.Transactional;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class SpringbootDemoApplicationTests {
+@Service
+public class PersonService {
+    @Autowired
+    PersonRepository personRepository;
 
     @Autowired
     ApplicationContext applicationContext;
@@ -20,17 +21,22 @@ public class SpringbootDemoApplicationTests {
     @Autowired
     DataSourceProperties dataSourceProperties;
 
-    @Test
-    public void testDataSource() throws Exception {
+    /**
+     * 保存
+     * @param person
+     * @return
+     */
+    @Transactional
+    public Person save(Person person) {
         // 获取配置的数据源
         DataSource dataSource = applicationContext.getBean(DataSource.class);
         // 查看配置数据源信息
         System.out.println(dataSource);
         System.out.println(dataSource.getClass().getName());
         System.out.println(dataSourceProperties);
-    }
-    @Test
-    public void contextLoads() {
-    }
+        Person p = personRepository.save(person);
 
+        return p;
+
+    }
 }
